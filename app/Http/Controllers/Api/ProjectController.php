93416edff9ed14cfac0a483396b43ac5d5bb0073
Dan848 <daniele.ceravolo15@gmail.com>
2023-06-15 09:05:16 +0200

    add show route

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -11,11 +11,28 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::with('technology', 'dev_languages')->paginate(5);
+        $projects = Project::with('technology', 'dev_languages')->paginate(6);
 
         return response()->json([
             'success' => true,
             'results' => $projects
         ]);
+    }
+
+    public function show($slug)
+    {
+        $project = Project::with('technology', 'dev_languages')->where('slug', $slug)->first();
+
+        if ($project) {
+            return response()->json([
+                'success' => true,
+                'results' => $project
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'results' => 'Project does not exist!'
+            ]);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,4 @@ use App\Http\Controllers\Api\ProjectController;
 
 
 Route::get('/projects', [ProjectController::class, 'index']);
+Route::get('/projects/{slug}', [ProjectController::class, 'show']);
